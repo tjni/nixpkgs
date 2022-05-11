1d1d7cda6b59fb15597f95dafe0169fa6dcb7c2c
Theodore Ni <3806110+tjni@users.noreply.github.com>
2022-05-11 11:52:17 -0500

    gecode: fix build

--- pkgs/development/libraries/gecode/3.nix
+++ pkgs/development/libraries/gecode/3.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, perl }:
+{ lib, stdenv, fetchpatch, fetchurl, perl }:
 
 stdenv.mkDerivation rec {
   pname = "gecode";
@@ -8,6 +8,14 @@ stdenv.mkDerivation rec {
     url = "http://www.gecode.org/download/${pname}-${version}.tar.gz";
     sha256 = "0k45jas6p3cyldgyir1314ja3174sayn2h2ly3z9b4dl3368pk77";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "fix_no_viable_overloaded_equals";
+      url = "https://github.com/Gecode/gecode/pull/74/commits/c810c96b1ce5d3692e93439f76c4fa7d3daf9fbb.patch";
+      sha256 = "";
+    })
+  ];
 
   nativeBuildInputs = [ perl ];
 
--- pkgs/development/libraries/gecode/default.nix
+++ pkgs/development/libraries/gecode/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, bison, flex, perl, gmp, mpfr, enableGist ? true, qtbase }:
+{ lib, stdenv, fetchFromGitHub, fetchpatch, bison, flex, perl, gmp, mpfr, enableGist ? true, qtbase }:
 
 stdenv.mkDerivation rec {
   pname = "gecode";
@@ -10,6 +10,15 @@ stdenv.mkDerivation rec {
     rev = "release-${version}";
     sha256 = "0b1cq0c810j1xr2x9y9996p894571sdxng5h74py17c6nr8c6dmk";
   };
+
+  patches = [
+    # This can be removed once version 6.3.0 is released.
+    (fetchpatch {
+      name = "fix_no_viable_overloaded_equals";
+      url = "https://github.com/Gecode/gecode/pull/74/commits/c810c96b1ce5d3692e93439f76c4fa7d3daf9fbb.patch";
+      sha256 = "";
+    })
+  ];
 
   enableParallelBuilding = true;
   dontWrapQtApps = true;
