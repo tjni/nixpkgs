84f69a8f797d20e8858693b1a7ce0e7e6edfaae0
Theodore Ni <3806110+tjni@users.noreply.github.com>
2023-08-07 11:41:53 -0700

    python3.pkgs.captcha: 0.4 -> 0.5.0

--- pkgs/development/python-modules/captcha/default.nix
+++ pkgs/development/python-modules/captcha/default.nix
@@ -1,32 +1,36 @@
 { lib
 , fetchFromGitHub
 , buildPythonPackage
-, nose
 , pillow
-, wheezy-captcha
+, pytestCheckHook
+, setuptools
+, wheel
 }:
 
 buildPythonPackage rec {
   pname = "captcha";
-  version = "0.4";
-  format = "setuptools";
+  version = "0.5.0";
+  format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "lepture";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-uxUjoACN65Cx5LMKpT+bZhKpf2JRSaEyysnYUgZntp8=";
+    hash = "sha256-TPPuf0BRZPSHPSF0HuGxhjhoSyZQ7r86kSjkrztgZ5w=";
   };
+
+  nativeBuildInputs = [
+    setuptools
+    wheel
+  ];
 
   propagatedBuildInputs = [ pillow ];
 
   pythonImportsCheck = [ "captcha" ];
 
-  nativeCheckInputs = [ nose wheezy-captcha ];
-
-  checkPhase = ''
-    nosetests -s
-  '';
+  nativeCheckInputs = [ 
+    pytestCheckHook
+  ];
 
   meta = with lib; {
     description = "A captcha library that generates audio and image CAPTCHAs";
