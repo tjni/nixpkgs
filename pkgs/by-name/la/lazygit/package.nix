7f4c4567531aa555cf35b5871de18a8e633205aa
Jost Alemann <alemann@posteo.com>
2026-09-13 09:07:27 +0200

    lazygit: 0.65.0 -> 0.65.1
    Changelog: https://github.com/jesseduffield/lazygit/releases/tag/v0.65.1
    Diff: https://github.com/jesseduffield/lazygit/compare/v0.65.0...v0.65.1

--- pkgs/by-name/la/lazygit/package.nix
+++ pkgs/by-name/la/lazygit/package.nix
@@ -8,13 +8,13 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "lazygit";
-  version = "0.65.0";
+  version = "0.65.1";
 
   src = fetchFromGitHub {
     owner = "jesseduffield";
     repo = "lazygit";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-W/Z/BnA5L8tXYrKn3bnekNnx7bEaaJRoHSGAeygcK0E=";
+    hash = "sha256-vpvjywx8PeKuAVWfYfG6MUfSddOwZupzRhPYwrTWEmw=";
   };
 
   vendorHash = null;
